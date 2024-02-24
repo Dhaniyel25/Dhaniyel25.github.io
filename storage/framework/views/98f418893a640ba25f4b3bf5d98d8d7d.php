221d8d2ef93619310502206c19c47408003e5104
Dhaniyel25 <daniel.okta.a@mail.ukrim.ac.id>
2024-02-24 21:26:10 +0700

    first commit

---
+++ storage/framework/views/98f418893a640ba25f4b3bf5d98d8d7d.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    ini profile user
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('backend/layout/main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\latihan\resources\views/backend/content/profile.blade.php ENDPATH**/ ?>
